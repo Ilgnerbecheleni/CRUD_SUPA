f7edd4a72db144a49f991234afecc9b70524d730
ilgner becheleni <ilgner.gui@gmail.com>
2023-02-17 10:10:17 -0300

    aula-5

--- scripts/aplicacao.js
+++ scripts/aplicacao.js
@@ -1,6 +1,6 @@
 const card = document.getElementById('cards')
 const form = document.getElementById('formContato');
-
+let condicao = true ;
 
 
 
@@ -8,6 +8,9 @@ const form = document.getElementById('formContato');
 
 
 async function loadData () {
+
+
+
   let { data: Contatos, error } = await _supabase.from('Contatos').select('*')
 
   console.table(Contatos)
@@ -23,7 +26,7 @@ async function loadData () {
         <div class="card-body">
             <h5 class="card-title">${contato.Nome}</h5>
             <h5 class="card-text">${contato.Telefone}</h5>
-            <button class="btn btn-primary">Editar</button>
+            <button class="btn btn-primary" onClick='prepareData(${contato.id})'>Editar</button>
             <button class="btn btn-danger">Apagar</button>
         </div>
     </div>`
@@ -47,7 +50,14 @@ form.addEventListener('submit', async (e) => {
     }
 
     console.log(data);
- insertData(data);
+    if(condicao){
+        insertData(data);
+    }else{
+        let id = parseInt(form.hiddenId.value);
+        updateData(id,data);
+        condicao=true;
+    }
+
  form.reset() ; 
  
 }); 
@@ -65,4 +75,46 @@ console.log("dado salvo com sucesso!");
 loadData();
 }
 }
+
+async function prepareData(id){
+
+    let { data: Contatos, error } = await _supabase
+    .from('Contatos')
+    .select().eq('id', id).limit(1)
+    if(error){
+        console.log(error);
+        return;
+    }else{
+       
+        console.log(Contatos[0]["Nome"]);
+        form.hiddenId.value = Contatos[0]["id"]
+          form.nome.value = Contatos[0]["Nome"] ;
+          form.telefone.value = Contatos[0]["Telefone"] ;
+          condicao = false ;
+    }
+    
+}
+
+
+
+async function updateData(id,dado){
+
+ 
+    const { data, error } = await _supabase
+    .from('Contatos')
+    .update(dado)
+    .eq('id', id)
+
+   
+if(error){
+    console.log(error);
+    
+    return 
+}
+loadData();
+
+
+}
+
+updateData(1);
 
